011cd78b1a8dbd585a582f59ba1546e7d8fcefca
wodooWindows <insoul_net@qq.com>
2019-05-15 22:57:03 +0800

    实现简单授权，无权限跳转页面

--- src/main/java/com/xywei/springsecurity/web/IndexPage.java
+++ src/main/java/com/xywei/springsecurity/web/IndexPage.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexPage {
 
-	@RequestMapping(value = { "/"})
+	@RequestMapping(value = { "/" })
 	public String toIndex(Model model) {
 		model.addAttribute("username", "wei");
 		return "index";
+	}
+
+	@RequestMapping(value = { "/error/denied" })
+	public String deniedPage() {
+		return "error/403";
 	}
 }
