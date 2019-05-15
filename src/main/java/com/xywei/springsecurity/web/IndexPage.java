330ffad761cf82f9c630d70729a770c99b3f5640
wodooWindows <insoul_net@qq.com>
2019-05-15 17:38:44 +0800

    initial

---
+++ src/main/java/com/xywei/springsecurity/web/IndexPage.java
@@ -0,0 +1,15 @@
+package com.xywei.springsecurity.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexPage {
+
+	@RequestMapping(value = { "/"})
+	public String toIndex(Model model) {
+		model.addAttribute("username", "wei");
+		return "index";
+	}
+}
